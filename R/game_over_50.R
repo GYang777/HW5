436d4706c3e8cc9750c3c6c78b7788be002e9ab1
GYang777 <60439986+GYang777@users.noreply.github.com>
2020-03-04 17:22:42 -0500

    Update game_over_50.R

--- R/game_over_50.R
+++ R/game_over_50.R
@@ -9,9 +9,7 @@
 
 
 games_over_50 <- function(year) {
-  a <- Seasons_Stats_NBA
-  dplyr::function_dataset_1 <- filter(a, Year==year)
-  data.frame(function_dataset_1)
+  dplyr::function_dataset_1 <- filter(Seasons_Stats_NBA, Year==year)
   dplyr::filter(function_dataset_1, G>50)
 }
 
